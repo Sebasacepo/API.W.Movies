f63da5a52ed479835cc558a4b34260cbd9070343
sebasacepo <sebasacepo@hotmail.com>
2025-10-28 20:42:32 -0500

    Add project files.

---
+++ API.W.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+       
+         [Key] //decorator
+         public int Id { get; set; }
+         public DateTime CreateDate { get; set; }
+
+         public DateTime ModifiedDate { get; set; }
+        
+    }
+}
---
+++ API.W.Movies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class Category:AuditBase
+    {
+        [Required] //Este data annotation está indicando que este campo es obligatorio
+        [Display(Name = "Nombre de la categoría")]
+        public string Name { get; set; }
+    }
+}
