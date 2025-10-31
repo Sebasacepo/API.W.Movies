e47f4719ad10a04f640ad648f7f1719c10f759c2
sebasacepo <sebasacepo@hotmail.com>
2025-10-31 18:11:28 -0500

    Agregado parte del codigo minuto 28 video

---
+++ API.W.Movies/Repository/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using API.W.Movies.DAL;
+using API.W.Movies.DAL.Models;
+using API.W.Movies.Repository.IRepository;
+
+namespace API.W.Movies.Repository
+{        
+    public class CategoryRepository : ICategoryRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.W.Movies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.W.Movies.DAL.Models;
+
+namespace API.W.Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        //Aquí se pone la firma
+        Task<ICollection<Category>> GetCategoriesAsync(); //Me retorna una lista de categorías
+        Task<Category> GetCategoryAsync(int id); //Me retorna una categoría por su Id
+        Task<bool> CategoryExistsByIdAsync(int id); //Me dice si una categoría existe por su Id
+        Task<bool> CategoryExistsByNameAsync(string name); //Me dice si una categoría existe por su Id
+        Task<bool> CreateCategoryAsync(Category category); //Me crea una categoría
+        Task<bool> UpdateCategoryAsync(Category category); //Me actualiza una categoría
+        Task<bool> DeleteCategoryAsync(int id); //Me elimina una categoría
+
+
+    }
+}
