ee4b6c34e9e4fea1e9a2944dbd411b669cbfbfa5
sebasacepo <sebasacepo@hotmail.com>
2025-11-11 22:01:05 -0500

    se agregó category service

--- API.W.Movies/DAL/Models/DTO/CategoryDto.cs
+++ API.W.Movies/DAL/Models/DTO/CategoryDto.cs
@@ -1,11 +1,16 @@
-﻿namespace API.W.Movies.DAL.Models.DTO
+﻿using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models.DTO
 {
     public class CategoryDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre dela")]
+        [MaxLength(100, ErrorMessage = "El número máximo de caracteres es 100.")]
         public string Name { get; set; }
         public DateTime CreateDate { get; set; }
-
         public DateTime? UpdateDate { get; set; }
 
     }
---
+++ API.W.Movies/Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using API.W.Movies.DAL.Models;
+using API.W.Movies.Repository.IRepository;
+using API.W.Movies.Services.IServices;
+using AutoMapper;
+
+namespace API.W.Movies.Services
+{
+    public class CategoryService: ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper) 
+        { 
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            var categories =  _categoryRepository.GetCategoriesAsync();
+            var categoriesDto = _mapper.Map<ICollection<CategoryDto>>(categories);
+        }
+
+        public Task<Category> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
